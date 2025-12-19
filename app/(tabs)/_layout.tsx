a111f688c763ac8bf8799f7e0fee3eade175c7b8
Prasad-D-Ware <prasadware42116@gmail.com>
2025-12-20 02:02:12 +0530

    feat: liquid glass for tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,48 @@
 import { useTheme } from "@/hooks/use-theme";
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from 'expo-router';
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from 'react';
 
 const TabLayout = () => {
     const { colors } = useTheme();
   return (
-    <Tabs screenOptions={{
-        tabBarActiveTintColor : colors.primary,
-        tabBarInactiveTintColor : colors.textMuted,
-        tabBarStyle : {
-            backgroundColor : colors.surface,
-            borderTopWidth : 1,
-            borderBlockColor : colors.border,
-            paddingTop : 10,
-        },
-        tabBarLabelStyle : {
-            fontSize  : 12
-        },
-        headerShown : false
-    }}>
-        <Tabs.Screen name="index" options={{
-            title: "Todos",
-            tabBarIcon : ({color, size}) => (
-                <Ionicons name="flash-outline" color={color} size={size}/>
-            ),
-        }}/>
-          <Tabs.Screen name="settings" options={{
-            title: "Settings",
-            tabBarIcon : ({color, size}) => (
-                <Ionicons name="settings-outline"  color={color} size={size}/>
-            ),
-        }}/>
+    // <Tabs screenOptions={{
+    //     tabBarActiveTintColor : colors.primary,
+    //     tabBarInactiveTintColor : colors.textMuted,
+    //     tabBarStyle : {
+    //         backgroundColor : colors.surface,
+    //         borderTopWidth : 1,
+    //         borderBlockColor : colors.border,
+    //         paddingTop : 10,
+    //     },
+    //     tabBarLabelStyle : {
+    //         fontSize  : 12
+    //     },
+    //     headerShown : false
+    // }}>
+    //     <Tabs.Screen name="index" options={{
+    //         title: "Todos",
+    //         tabBarIcon : ({color, size}) => (
+    //             <Ionicons name="flash-outline" color={color} size={size}/>
+    //         ),
+    //     }}/>
+    //       <Tabs.Screen name="settings" options={{
+    //         title: "Settings",
+    //         tabBarIcon : ({color, size}) => (
+    //             <Ionicons name="settings-outline"  color={color} size={size}/>
+    //         ),
+    //     }}/>
 
-    </Tabs>
+    // </Tabs>
+    <NativeTabs>
+        <NativeTabs.Trigger name="index">
+            <Label >Todos</Label>
+            <Icon sf={"checklist"} />
+        </NativeTabs.Trigger>
+        <NativeTabs.Trigger name="settings">
+            <Label >Settings</Label>
+            <Icon sf={"gear"} />
+        </NativeTabs.Trigger>
+    </NativeTabs>
   )
 }
 
